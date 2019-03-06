0638a138cb5d59d5610910970ae7cc3505b538ed
HoggenMari <marius.hoggenmueller@campus.lmu.de>
2019-03-06 18:47:54 +1100

    * fixed build script to include external libraries

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,7 +1,13 @@
 package core;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 public class Main {
+    static Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        BasicConfigurator.configure();
+        logger.info("Hello World");          // the old SysO-statement
     }
 }
